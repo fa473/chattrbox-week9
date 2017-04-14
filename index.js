a2acd08e3bfaf108afa5f937e089d37e590cd9de
fa473 <home@homes-MacBook-Pro.local>
2017-04-13 19:20:18 -0700

    ch 16,17

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
+var mime = require('mime');
+var wss = require('./websockets-server');
 
 var handleError = function(err, res) {
     res.writeHead(404);
@@ -15,6 +17,7 @@ var server = http.createServer(function(req, res) {
             handleError(err, res);
             return;
         } else {
+            res.setHeader('content-type', mime.lookup(filePath));
             res.end(data);
         }
     });
